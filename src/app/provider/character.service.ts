b32fd7e751dd6f51b3d99a324a64ba3d68459024
Guilherme Souza <guillehs96@gmail.com>
2022-05-31 21:06:04 -0300

    Finalizado

--- src/app/provider/character.service.ts
+++ src/app/provider/character.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { promise } from 'protractor';
 import { ServiceService } from '../api/service.service';
 import { PaginationComponent } from './../util/pagination/pagination.component';
 
@@ -12,13 +11,19 @@ export class CharacterService {
 
   public getCharacterById(id: number){
     return new Promise((ret) => {
-      this.service.getDados('/v1/public/characters/{characterId}' + id, '').then((data: any) => {
-          if(data && data.data && data.data.results){
-            ret(data.data.results);
-          } else {
 
-            ret([]);
-          }
+      this.service.getDados('v1/public/characters/' + id, '').then((data: any) => {
+
+        if(data && data.data && data.data.results){
+          ret(data.data.results);
+
+        } else {
+          ret([]);
+        }
+
+      },
+      (err) => {
+        ret(false);
 
       })
     })
@@ -27,36 +32,52 @@ export class CharacterService {
   public getAllCharacters(pagination: PaginationComponent, filter: string){
     let strFilter = '';
 
+    if(filter){
+      strFilter = '&nameStartsWith=' + filter;
+    }
+
     let param = '&limit=' + pagination.getLimit() + '&offset=' + pagination.getOffset() + strFilter;
 
     return new Promise((ret) => {
-      this.service.getDados('/v1/public/characters', param).then((data: any) =>{
-        if(data ** data.data && data.data.results){
+      this.service.getDados('v1/public/characters', param).then((data:any) => {
+
+        if(data && data.data && data.data.results){
           this.updatePagination(pagination, data.data);
+
           ret(data.data.results);
-        
+
         } else {
           ret([]);
 
         }
-      })
+
+      },
+      (err)=> {
+        ret(false);
+
+      });
     })
+
   }
 
-  public getComicsByCharacter(id: number){
+  public getComicsByCharacter(character: any){
     return new Promise((ret) => {
-      this.service.getDados('/v1/public/characters/'+ id + '/comics', '').then((data:any) => {
+      this.service.getDados('v1/public/characters/'+ character.id + '/comics', '').then((data: any) => {
+
         if(data && data.data && data.data.results){
           ret(data.data.results);
 
         } else {
           ret([]);
-
         }
+
+      },
+       (err) => {
+        ret(false);
+
       })
     })
   }
-  /* atualiza as informações de paginação */
   private updatePagination(pagination: PaginationComponent, data: any){
     pagination.setTotal(data.total);
     pagination.setLimit(data.limit);
